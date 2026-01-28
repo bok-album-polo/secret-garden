e9257c590aee2ae89082e3f7bf2c030855cb7d0b
secret-garden-coder <secret-garden-coder@mail.com>
2026-01-28 19:44:50 +0300

    refactor(core): replace Router logic with direct controller handling

--- source/index.php
+++ source/index.php
@@ -2,10 +2,9 @@
 
 use App\Core\ErrorHandler;
 use App\Core\Session;
-use App\Core\Router;
 
-use App\Controllers\ContactController;
-use App\Controllers\RegistrationController;
+use App\Controllers\SecretDoorController;
+use App\Controllers\SecretPageController;
 
 require_once __DIR__ . '/src/autoloader.php';
 require_once __DIR__ . '/config/config.php'; // defines PAGES, SECRET_DOOR, SECRET_PAGE, etc.
@@ -14,25 +13,28 @@ require_once __DIR__ . '/config/config.php'; // defines PAGES, SECRET_DOOR, SECR
 ErrorHandler::initialize();
 Session::initialize();
 Session::runAuthenticationSequence();
-// Initialize router
-$router = new Router();
 
-// Register routes (slugs map to controllers/actions)
-$router->add('contact-us', ContactController::class, 'index');
-$router->add('registration', RegistrationController::class, 'index');
-$router->add('registration-summary', RegistrationController::class, 'registrationSummary');
-
-// for all static views
-$router->add('*', \App\Controllers\PageController::class, 'show');
-
-
-// Decide a route based on config
 if (ENABLE_PRETTY_URLS) {
     $uri = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
-    $route = $uri ?: 'home';
+    $page = $uri ?: 'home';
 } else {
-    $route = $_GET['page'] ?? 'home';
+    $page = $_GET['page'] ?? 'home';
 }
 
-// Dispatch to the appropriate controller
-$router->dispatch($route);
+switch ($page) {
+    case SECRET_DOOR:
+        if ($_SESSION['pk_auth']) {
+            $controller = new SecretPageController();
+            $controller->index();
+        } else {
+            // secret door
+            $controller = new SecretDoorController();
+            $controller->index();
+        }
+        break;
+    default:
+        //render page
+        $controller = new \App\Controllers\GenericPageController();
+        $controller->show($page);
+        break;
+}
