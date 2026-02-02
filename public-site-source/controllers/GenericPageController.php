1228211d478e307a57851d490d51537782c48a45
secret-garden-coder <secret-garden-coder@mail.com>
2026-02-02 19:51:06 +0300

    refactor(controller): remove obsolete pk-reset route logout logic

--- public-site-source/controllers/GenericPageController.php
+++ public-site-source/controllers/GenericPageController.php
@@ -6,9 +6,6 @@ class GenericPageController extends Controller
 {
     public function show(string $route): void
     {
-        if ($route == 'pk-reset') {
-            Session::logout();
-        }
         // Build views path based on the route name
         $viewPath = 'pages/' . $route;
         $this->render($viewPath, [
