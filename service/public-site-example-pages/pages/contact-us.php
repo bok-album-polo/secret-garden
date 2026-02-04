1f1e3b8bd5b02af9d65d0494592b694f86ffe67e
secret-garden-coder <secret-garden-coder@mail.com>
2026-02-04 15:36:56 +0300

    ✨ feat(pages): refactor form rendering on contact and registration pages
    - Replace inline form generation with `renderFields` method for cleaner templates
    - Pass dynamic default values to `renderFields` in the registration page

--- service/public-site-example-pages/pages/contact-us.php
+++ service/public-site-example-pages/pages/contact-us.php
@@ -3,33 +3,10 @@
     <p>Let's grow something beautiful together.</p>
 
     <form action="" method="POST" enctype="multipart/form-data">
-        <?php
+        <?=
         /** @var array<int,array<string,mixed> $fields */
-        foreach ($fields as $field): ?>
-            <div class="form-field" style="margin-bottom:1em;">
-                <label><?= htmlspecialchars($field['label'], ENT_QUOTES, 'UTF-8') ?></label><br>
-
-                <?php if ($field['html_type'] === 'textarea'): ?>
-                    <textarea name="<?= htmlspecialchars($field['name'], ENT_QUOTES, 'UTF-8') ?>"
-                              rows="4"
-                              <?= $field['required'] ? 'required' : '' ?>
-                            <?= isset($field['maxlength']) ? 'maxlength="' . (int)$field['maxlength'] . '"' : '' ?>></textarea>
-                    <br>
-                <?php elseif ($field['html_type'] === 'file'): ?>
-                    <input type="file" name="<?= htmlspecialchars($field['name'], ENT_QUOTES, 'UTF-8') ?>"
-                            <?= $field['required'] ? 'required' : '' ?>><br>
-                <?php else: ?>
-                    <input type="<?= htmlspecialchars($field['html_type'], ENT_QUOTES, 'UTF-8') ?>"
-                           name="<?= htmlspecialchars($field['name'], ENT_QUOTES, 'UTF-8') ?>"
-                            <?= $field['required'] ? 'required' : '' ?>
-                            <?= isset($field['maxlength']) ? 'maxlength="' . (int)$field['maxlength'] . '"' : '' ?>><br>
-                <?php endif; ?>
-
-                <?php if (!empty($field['help_text'])): ?>
-                    <small><?= htmlspecialchars($field['help_text'], ENT_QUOTES, 'UTF-8') ?></small><br>
-                <?php endif; ?>
-            </div>
-        <?php endforeach; ?>
+        \App\Controllers\Controller::renderFields($fields)
+        ?>
 
         <div>
             <button type="submit">Send Message</button>
--- service/public-site-example-pages/pages/registration.php
+++ service/public-site-example-pages/pages/registration.php
@@ -11,45 +11,13 @@ $dispatched_display_name = $user['display_name'] ?? '';
     <p><strong>Authorized Access Only</strong></p>
 
     <form action="" method="POST" enctype="multipart/form-data">
-        <?php
+        <?=
         /** @var array<int,array<string,mixed> $fields */
-        foreach ($fields as $field): ?>
-            <div style="margin-bottom:1em;">
-                <label><?= htmlspecialchars($field['label'], ENT_QUOTES, 'UTF-8') ?></label><br>
-
-                <?php
-                // Decide default value based on field name
-                $defaultValue = '';
-                if ($field['name'] === 'username') {
-                    $defaultValue = $dispatched_username;
-                } elseif ($field['name'] === 'displayname') {
-                    $defaultValue = $dispatched_display_name;
-                }
-                ?>
-
-                <?php if ($field['html_type'] === 'textarea'): ?>
-                    <textarea name="<?= htmlspecialchars($field['name'], ENT_QUOTES, 'UTF-8') ?>"
-                              <?= $field['required'] ? 'required' : '' ?>
-                            <?= isset($field['maxlength']) ? 'maxlength="' . (int)$field['maxlength'] . '"' : '' ?>><?= htmlspecialchars($defaultValue, ENT_QUOTES, 'UTF-8') ?>
-                    </textarea>
-                    <br>
-                <?php elseif ($field['html_type'] === 'file'): ?>
-                    <input type="file"
-                           name="<?= htmlspecialchars($field['name'], ENT_QUOTES, 'UTF-8') ?>"
-                            <?= $field['required'] ? 'required' : '' ?>><br>
-                <?php else: ?>
-                    <input type="<?= htmlspecialchars($field['html_type'], ENT_QUOTES, 'UTF-8') ?>"
-                           name="<?= htmlspecialchars($field['name'], ENT_QUOTES, 'UTF-8') ?>"
-                           value="<?= htmlspecialchars($defaultValue, ENT_QUOTES, 'UTF-8') ?>"
-                            <?= $field['required'] ? 'required' : '' ?>
-                            <?= isset($field['maxlength']) ? 'maxlength="' . (int)$field['maxlength'] . '"' : '' ?>><br>
-                <?php endif; ?>
-
-                <?php if (!empty($field['help_text'])): ?>
-                    <small><?= htmlspecialchars($field['help_text'], ENT_QUOTES, 'UTF-8') ?></small><br>
-                <?php endif; ?>
-            </div>
-        <?php endforeach; ?>
+        \App\Controllers\Controller::renderFields($fields, [
+                'username' => $dispatched_username,
+                'displayname' => $dispatched_display_name,
+        ]);
+        ?>
 
         <div>
             <button type="submit">Submit</button>
