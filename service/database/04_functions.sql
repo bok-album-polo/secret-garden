58a77054f9ecfba7727f2b91bd03a849ea89e41d
bok-album-polo <bok-album-polo@duck.com>
2026-02-05 14:08:46 +0000

    secret_room_submission_get: merge into 04_functions.sql. Will think about how to split out again later if necessary.

--- service/database/04_functions.sql
+++ service/database/04_functions.sql
@@ -89,6 +89,31 @@ BEGIN
 END;
 $$;
 
+-- Function: get_secret_room_submission(p_username VARCHAR)
+-- Description: Retrieves the most recent secret room submission for a given username, ensuring domain security.
+-- Used in readwrite mode to allow users to view their own submissions (vs writeonly mode where they cannot).
+
+CREATE OR REPLACE FUNCTION secret_room_submission_get(
+    p_username VARCHAR
+)
+RETURNS SETOF secret_room_submissions
+LANGUAGE plpgsql
+STABLE
+SECURITY DEFINER
+SET search_path = public, pg_temp
+AS $$
+BEGIN
+    RETURN QUERY
+    SELECT sr.*
+    FROM secret_room_submissions sr
+    JOIN users u ON sr.username = u.username
+    WHERE sr.username = p_username
+      AND u.domain = SESSION_USER::VARCHAR
+    ORDER BY sr.created_at DESC
+    LIMIT 1;
+END;
+$$;
+
 CREATE OR REPLACE FUNCTION ip_ban_ban(
     p_ip_address INET,
     p_reason TEXT,
